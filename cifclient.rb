e3fb397a28ad41758aba7a01011ce28995dde3ee
GambitK <alfred.rapozo@gmail.com>
2015-03-13 12:12:11 -0400

    Update cifclient.rb

--- cifclient.rb
+++ cifclient.rb
@@ -9,25 +9,47 @@ class Cifclient
     def initialize(remote, token)
       @remote = remote
       @token = token
-      @logger = Logger.new(STDOUT)      
-    end
-
-    def print
+      @logger = Logger.new(STDOUT)
       @handle = HTTPClient.new(:agent_name => 'rb-cif-sdk/0.0.1')
-      @handle.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE     
-      @logger.debug{@remote + ", " + @token}
-      @logger.debug{@handle.ssl_config.verify_mode}
-      uri = URI(@remote + '/ping')
-      headers = {
+      @handle.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      @headers = {
         'Authorization' => 'Token token=' + @token,
         'Content-Type' => 'application/json'
       }
-      @logger.debug{"uri: #{uri}"}
-      res = @handle.get(uri,params={},headers)
-      @logger.debug{res.status_code}
-      @logger.debug{JSON.parse(res.body)}
+    end
+
+    def make_request(uri='',type='get',params={})
+
+      @logger.debug{@remote + ", " + @token}
+
+      case type
+      when 'get'
+        @logger.debug{"uri: #{uri}"}
+        response = @handle.get(uri,params,@headers)
+        @logger.debug{response.status_code}
+        @logger.debug{JSON.parse(response.body)}
+      end
+    end
+
+    def search(filters = {},limit=1,sort='lasttime')
+      filters[:limit] = limit
+      filters[:sort] = sort
+      @logger.debug{filters}
+      uri = URI(@remote + '/observables')
+      res = make_request(uri,'get',filters)
+    end
+
+    def ping
+      uri = URI(@remote + '/ping')
+
+      start = Time.now
+      res = make_request(uri)
+      return nil unless(res)
+      return (Time.now() - start)
     end
 end
 
 cif = Cifclient.new("https://10.0.0.10", "bc6049e7c5d479db663e72cc3309dfe7d9dff7e734f7397d5886169ca8a9711d")
-cif.print
+#puts cif.ping
+cif.search(:query=>'61.240.144.116')
